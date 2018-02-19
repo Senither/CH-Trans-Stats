df83e32628f77d193d50bdc507e4a37f21f9f8a8
Alexis Tan <alexis@sen-dev.com>
2018-02-19 05:11:05 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+
+// Nothing is here yet, come back later, maybe there
+// will be something then... Most likely not...
+console.log('Hello, World!');
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width">
+
+    <title>Clicker Heroes Transcendens Statistics</title>
+
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <script type="text/javascript" src="js/app.js"></script>
+</head>
+<body>
+
+    <h1>Hello, World!</h1>
+
+</body>
+</html>
